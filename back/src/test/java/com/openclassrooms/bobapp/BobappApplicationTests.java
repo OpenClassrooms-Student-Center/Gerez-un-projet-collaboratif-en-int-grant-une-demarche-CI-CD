976aa3720bc8c26fae74d886254db73bf011b34d
Louvet Thomas <thomas.louvet@laposte.net>
2023-09-05 20:31:58 +0200

    test: :white_check_mark: adding deps on backend testing file to validate test is taken into account

--- back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
+++ back/src/test/java/com/openclassrooms/bobapp/BobappApplicationTests.java
@@ -2,6 +2,7 @@ package com.openclassrooms.bobapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class BobappApplicationTests {

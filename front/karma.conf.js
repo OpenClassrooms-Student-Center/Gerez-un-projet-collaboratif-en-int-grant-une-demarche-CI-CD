8b9f3dde5241cd6431a8fa311d23e5735c6dbf77
NathanOllichon <ollichon.nathan@gmail.com>
2024-01-08 13:17:28 +0100

    Update karma.conf.js

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/bobapp'),
       subdir: '.',
       reporters: [
-        { type: 'html' },
+        { type: 'lcov' },
         { type: 'text-summary' }
       ]
     },
